e792e8641a0318af908da79c4b34b760ab0c987e
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-15 10:16:11 +0200

    Update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 export const getUsers = async (req, res) => {
   try {
     const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    return res.status(200).json(users);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get users!" });
@@ -17,7 +17,7 @@ export const getUser = async (req, res) => {
     const user = await prisma.user.findUnique({
       where: { id },
     });
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get user!" });
@@ -50,7 +50,7 @@ export const updateUser = async (req, res) => {
 
     const { password: userPassword, ...rest } = updatedUser;
 
-    res.status(200).json(rest);
+    return res.status(200).json(rest);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to update users!" });
@@ -126,10 +126,10 @@ export const profilePosts = async (req, res) => {
     });
 
     const savedPosts = saved.map((item) => item.post);
-    res.status(200).json({ userPosts, savedPosts });
+    return res.status(200).json({ userPosts, savedPosts });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to get profile posts!" });
+    return res.status(500).json({ message: "Failed to get profile posts!" });
   }
 };
 
