b77a5945e273f90a0a6a37f32e8bd4c38d44f074
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-15 10:16:06 +0200

    Update verifyToken.js

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -5,8 +5,18 @@ export const verifyToken = (req, res, next) => {
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
+  console.log("Token found, verifying...");
+
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+    if (err) {
+
+      console.log("Token verification failed, sending 403 response.");
+      return res.status(403).json({ message: "Token is not Valid!" });
+
+      
+    }
+
+    console.log('Token verified successfully.');
     req.userId = payload.id;
 
     next();
