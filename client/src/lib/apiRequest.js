b5a04d9fe4fb2e31fa1e9565295ea2760ae7b141
Goodness (Themba) Chauke <69904835+Jakaza@users.noreply.github.com>
2026-03-26 10:45:29 +0200

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: 'http://localhost:8800/api',
+  baseURL: 'https://mzansi-real-estate-api.onrender.com/api',
   withCredentials: true,
 });
 
