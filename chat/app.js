5d7f7dc4eb04278f3faac8c26aa24ae1a211ea97
Goodness (Themba) Chauke <69904835+Jakaza@users.noreply.github.com>
2026-03-26 10:48:26 +0200

    Update app.js

--- chat/app.js
+++ chat/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "https://mzansi-real-estate.vercel.app",
+    origin: "mzansi-real-estate-git-main-earnclicks-projects.vercel.app",
   },
 });
 
